d41f8c31c0d3fb18702cff963857b830921144ae
orasundev <orasundev@gmail.com>
2021-02-14 13:06:29 +0500

    Good Source 1402

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+        <a href="/"> React Shopping cart</a>
+      </header>
+      <main>Main Section.</main>
+      <footer>All Right Reserv.</footer>
+    </div>
+  );
+}
+
+export default App;
